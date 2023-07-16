274232451aa238668836e05bbb7d103365d19500
Muhammad hassan <hassanhanjra900@gmail.com>
2023-07-16 10:49:48 -0400

    first-commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+# An Ai academic advisor
---
+++ test.py
@@ -0,0 +1,8 @@
+targetlinks = [
+    "https://online.nvcc.edu/faqs.html",
+    "https://www.nvcc.edu/medical/apply/steps/faq.html",
+    "https://www.nvcc.edu/campus-auxiliary-services/faq.html",
+    "https://online.nvcc.edu/testinginfo.html",
+    "https://www.nvcc.edu/academic-tools/student-financial-faq.html",
+    "https://online.nvcc.edu/fservices/index.html#panel2", ]
+import requests
