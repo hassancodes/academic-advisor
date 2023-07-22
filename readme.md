16e063dfc3c3fb47ea763d56a940b0d3650c2543
Muhammad hassan <hassanhanjra900@gmail.com>
2023-07-22 06:35:51 -0400

    adding QnAs

--- readme.md
+++ readme.md
@@ -1,2 +1,17 @@
 # An Ai academic advisor
 Revolutionalizing the way academic advising is done.
+
+
+----------------------------------------------------
+# DataStorage for Training
+
+pattern: 
+{
+    QnAs : {
+    {question:answer},
+    {question:answer}
+    {question:answer}
+    {question:answer}
+
+}
+}
--- test.py
+++ test.py
@@ -1,8 +1,46 @@
+import json
+import requests
+from bs4 import BeautifulSoup
+import pprint
+import unicodedata
+
+
+
 targetlinks = [
     "https://online.nvcc.edu/faqs.html",
     "https://www.nvcc.edu/medical/apply/steps/faq.html",
     "https://www.nvcc.edu/campus-auxiliary-services/faq.html",
     "https://online.nvcc.edu/testinginfo.html",
-    "https://www.nvcc.edu/academic-tools/student-financial-faq.html",
-    "https://online.nvcc.edu/fservices/index.html#panel2", ]
-import requests
+    "https://online.nvcc.edu/fservices/index.html", ]
+
+# total 54 panesl on this link 
+# first we will get all the title of questions
+# then their answers, next will will make them in a json file
+questions_list =[]
+answer_list = []
+currentlink = "https://online.nvcc.edu/fservices/index.html"
+req = requests.get(currentlink)
+soup = BeautifulSoup(req.content, "lxml")
+mydict = {} 
+# mydict = {'https://online.nvcc.edu/faqs.html': {'How do I apply to teach for NOVA Online?': 'If you are qualified to teach online and are interested in teaching for us, please send a resume/CV and cover letter to\xa0TeachOnline@nvcc.edu.\xa0 Your experience and credentials will be reviewed, you will be contacted for further information if necessary, and your background will be examined to see if you might fit with our current staffing needs.\xa0 If your experience does fit our current needs, NOVA Online will put you in contact with an Academic Dean on one of the NOVA campuses, who will interview you and complete the actual hiring process.'}}
+for i in range(1,13):
+    string = f"#panel{i}"
+    
+    # print(soup.find("a",{"href":string}).text) 
+    # print(soup.find("div",{'id':string[1:]}).text)
+    question =  str(unicodedata.normalize("NFKD",soup.find("a",{"href":string}).text.strip()).replace("\n","")).encode("ascii","ignore").decode().replace('\"',"'")
+    answer = str(unicodedata.normalize("NFKD",soup.find("div",{'id':string[1:]}).text.strip()).replace("\n","")).encode("ascii","ignore").decode().replace('\"',"'")
+    if len(mydict)==0:
+        mydict[currentlink] = {question:answer}
+    elif (len(mydict)==1):
+        mydict[currentlink].update({question:answer})
+    # print(question)
+    # print(answer)
+ 
+print(mydict)  
+with open("faqs.json", "r+") as fp:
+    a = json.load(fp)
+    # json.dump({}, fp)
+    a.update(mydict)
+    json.dump(a,fp)
+    # json.dump(mydict,fp)\
