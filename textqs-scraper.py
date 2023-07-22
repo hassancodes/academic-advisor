c9bf527606f1251058d6ea4573122e272b5eb2a6
Muhammad hassan <hassanhanjra900@gmail.com>
2023-07-22 13:40:37 -0400

    update: textqs..py

--- textqs-scraper.py
+++ textqs-scraper.py
@@ -5,8 +5,6 @@ import pprint
 import unicodedata
 
 links = [
-    "https://www.nvcc.edu/call-center/faq.html",
-    "https://www.nvcc.edu/transcripts/faq.html",
     "https://www.nvcc.edu/business-offices/faq.html",
     "https://www.nvcc.edu/parking/faq.html",
     "https://www.nvcc.edu/transfer/faq/",
@@ -21,21 +19,37 @@ links = [
 # https://www.nvcc.edu/employment/faq.html
 # https://www.nvcc.edu/academics/pathways/health-sciences/veterinary-technology/program/faq.html
 # https://www.nvcc.edu/osi/assessment/academic-program-review/faq.html
+questionlist=[]
+answerlist=[]
 
-currentlink = links[0]
+currentlink = "https://www.nvcc.edu/business-offices/faq.html"
 req = requests.get(currentlink)
 soup = BeautifulSoup(req.content, "lxml")
 mydict= {}
-for i in range(1,13):
-    string = f"#panel{i}"
+for i in range(1,8):
+    if len(questionlist)==0:
+       questionlist=[x.text for x in soup.find_all("span",{"class":"faq"})]
+    else:pass
     
+    string = f"faq{i}"
+    rawQnA = soup.find("p",{"id":string})
+    rawQnA.find("span").decompose()
+    answer = rawQnA.text
+    if len(mydict)==0:
+        mydict[currentlink]={questionlist[i-1]:answer}
+    else: 
+        mydict[currentlink].update({questionlist[i-1]:answer})
+
+print(mydict)
+
+    # print(soup.find("p",{"id":string}).text)
     # print(soup.find("a",{"href":string}).text) 
     # print(soup.find("div",{'id':string[1:]}).text)
-    question =  str(unicodedata.normalize("NFKD",soup.find("a",{"href":string}).text.strip()).replace("\n","")).encode("ascii","ignore").decode().replace('\"',"'")
-    answer = str(unicodedata.normalize("NFKD",soup.find("div",{'id':string[1:]}).text.strip()).replace("\n","")).encode("ascii","ignore").decode().replace('\"',"'")
-    if len(mydict)==0:
-        mydict[currentlink] = {question:answer}
-    elif (len(mydict)==1):
-        mydict[currentlink].update({question:answer})
+    # question =  str(unicodedata.normalize("NFKD",soup.find("a",{"href":string}).text.strip()).replace("\n","")).encode("ascii","ignore").decode().replace('\"',"'")
+    # answer = str(unicodedata.normalize("NFKD",soup.find("div",{'id':string[1:]}).text.strip()).replace("\n","")).encode("ascii","ignore").decode().replace('\"',"'")
+    # if len(mydict)==0:
+    #     mydict[currentlink] = {question:answer}
+    # elif (len(mydict)==1):
+    #     mydict[currentlink].update({question:answer})
 
 
